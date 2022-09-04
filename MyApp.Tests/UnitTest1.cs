919a080f0fadc46540f8fb57a779b32f3b7823fd
Amalie Holm <38892140+Miniim98@users.noreply.github.com>
2022-09-04 16:23:36 +0200

    App branch (#2)
    * app and test
    
    * Update other folder
    
    * Delete solution
    
    * Delete MyApp folder
    
    * new sln fil
    
    * Test and Program
    
    * Tested and implemented IsLeapYear.
    
    * Tested but not yet improved
    
    * user interface for IsLeapYear and error handling
    
    * test
    
    * test 2
    
    * test 3
    
    * test 4
    
    * test 5

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -3,8 +3,147 @@ namespace MyApp.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void first_rule_divided_by_4_true()
     {
+        // Arrange
+         
 
+        // Act
+        var answer = Program.IsLeapYear(2004);
+
+        // Assert
+        answer.Should().Be(true);
     }
+     [Fact]
+      public void first_rule_divided_by_4_false()
+    {
+        // Arrange
+
+        // Act
+        var answer = Program.IsLeapYear(2001);
+
+        // Assert
+        answer.Should().Be(false);
+    }
+        [Fact]
+      public void rule_divided_by_100()
+    {
+        // Arrange
+
+        // Act
+        var answer = Program.IsLeapYear(1700);
+
+        // Assert
+        answer.Should().Be(false);
+    }
+        [Fact]
+      public void rule_divided_by_400()
+    {
+        // Arrange
+
+        // Act
+        var answer = Program.IsLeapYear(1600);
+
+        // Assert
+        answer.Should().Be(true);
+    }
+
+            [Fact]
+      public void rule_divided_by_4()
+    {
+        // Arrange
+
+        // Act
+        var answer = Program.IsLeapYear(1864);
+
+        // Assert
+        answer.Should().Be(true);
+    }
+
+    [Fact]
+      public void test_of_user_input_and_output_yay()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        using var reader = new StringReader("1864");
+        Console.SetIn(reader);
+
+        // Act
+        Program.userInterfaceLeapYear();
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("yay!");
+    }
+
+    [Fact]
+      public void test_of_user_input_and_output_nay()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        using var reader = new StringReader("1866");
+        Console.SetIn(reader);
+
+        // Act
+        Program.userInterfaceLeapYear();
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("nay!");
+    }
+
+    [Fact]
+    public void test_input_is_not_an_int()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        using var reader = new StringReader("hey");
+        Console.SetIn(reader);
+
+        // Act
+        Program.userInterfaceLeapYear();
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Please input an integer!");
+    }
+
+    [Fact]
+    public void test_input_is_lower_than_1582()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        using var reader = new StringReader("1500");
+        Console.SetIn(reader);
+
+        // Act
+        Program.userInterfaceLeapYear();
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Please input a year equal or higher than 1582");
+    }
+
+      [Fact]
+    public void test_input_is_an_int_and_a_character()
+    {
+        // Arrange
+        using var writer = new StringWriter();
+        Console.SetOut(writer);
+        using var reader = new StringReader("1900a");
+        Console.SetIn(reader);
+
+        // Act
+        Program.userInterfaceLeapYear();
+
+        // Assert
+        var output = writer.GetStringBuilder().ToString().TrimEnd();
+        output.Should().Be("Please input an integer!");
+    }
+    
 }
+
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,2 +1,82 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MyApp;
+
+public class Program
+{
+    
+    static void Main(string[] args) 
+    {
+        HandleUserInteraction();
+    }
+
+    private static void HandleUserInteraction()
+    {
+        Console.WriteLine("Welcome to my leapyear check program!");
+        while (true)
+        {
+           Console.WriteLine("\n[1] check a year \n[2] exit"); 
+           var answer = Console.ReadLine();
+           
+           if (answer == "1")
+           {
+            Console.WriteLine("Please type the year that you want to check and press enter!");
+            userInterfaceLeapYear();
+           }
+           else if (answer == "2"){break;}
+           else
+           {
+            Console.WriteLine("Please input a valid option!");
+           }
+        }
+    }
+
+    public static void userInterfaceLeapYear()
+    {
+        var input = Console.ReadLine();
+        if (ErrorChecking(input))
+        {
+            var year = int.Parse(input);
+            if (IsLeapYear(year))
+            {
+                Console.WriteLine("yay!");
+                return;
+            }
+            Console.WriteLine("nay!");
+        }
+        
+        
+    }
+    public static bool IsLeapYear(int year) 
+    {
+        if (year % 4 == 0) 
+        {
+            if (year % 100 == 0)
+            {
+                return (year % 400 == 0);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private static bool ErrorChecking(string input)
+    {
+        int year;
+        try 
+        {
+            year = int.Parse(input);
+        } 
+        catch (Exception)
+        {
+           Console.WriteLine("Please input an integer!");
+           return false; 
+        }
+
+        if (year < 1582)
+        {
+            Console.WriteLine("Please input a year equal or higher than 1582");
+            return false;
+        }
+        return true;
+    }
+}
+
